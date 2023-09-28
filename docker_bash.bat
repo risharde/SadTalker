340fb9d277657e8b378847c5a2adf47eaa38a0a8
Risharde Ramnath <risharde@gmail.com>
2023-09-28 02:40:43 -0400

    New entry point in batch file

--- docker_bash.bat
+++ docker_bash.bat
@@ -1,1 +1,2 @@
-docker run -it risharde/ai:sadtalker-amd-latest bash
+REM docker run -it risharde/ai:sadtalker-amd-latest bash
+docker run --entrypoint /bin/bash -it risharde/ai:sadtalker-amd-latest
